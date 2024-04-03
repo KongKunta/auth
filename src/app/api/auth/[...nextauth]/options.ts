11c09617fa4f5fe5a9527e3d161149e7c4e4ddc1
KongKunta <kongkunta@yahoo.com>
2024-04-03 23:17:59 +0200

    added credentialsprovider and authorize to connect and find user in db

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,31 @@
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "../../../utils/dbConnect";
+import User from "../../../models/User";
 
 export const options: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+        const user = await User.findOne({ name: credentials.username });
+        if (user && user.password === credentials.password) {
+          console.log("user found");
+          return user;
+        } else {
+          return null;
+        }
+      },
     }),
   ],
 };
